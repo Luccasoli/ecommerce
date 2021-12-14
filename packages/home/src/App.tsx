cf5acf6609bbbe1f5ed330b52db2eca72b5a0626
Lucas Mesquita <lucasoliveira98@outlook.com>
2021-12-14 01:12:20 -0300

    feat: handling request to invidual mfe

--- packages/home/src/App.tsx
+++ packages/home/src/App.tsx
@@ -1,3 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { HomePage } from './pages/Home'
 
-export const App = () => <HomePage />
+export const App = () => (
+	<ChakraProvider>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="*" element={<Navigate to="/" />} />
+			</Routes>
+		</BrowserRouter>
+	</ChakraProvider>
+)
--- packages/search/src/App.tsx
+++ packages/search/src/App.tsx
@@ -1,3 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { SearchPage } from './pages/Search'
 
-export const App = () => <SearchPage />
+export const App = () => (
+	<ChakraProvider>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/search" element={<SearchPage />} />
+				<Route path="*" element={<Navigate to="/search" />} />
+			</Routes>
+		</BrowserRouter>
+	</ChakraProvider>
+)
