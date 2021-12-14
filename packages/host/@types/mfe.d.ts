415a34e7317d34b7455d03f591c1ef05c8f29a50
Lucas Mesquita <lucasoliveira98@outlook.com>
2021-12-14 01:26:56 -0300

    feat: adding types definitions to host

---
+++ packages/host/@types/mfe.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="react" />
+
+declare module 'home/Home' {
+	const HomePage: React.ComponentType
+
+	export { HomePage }
+}
+
+declare module 'search/Search' {
+	const SearchPage: React.ComponentType
+
+	export { SearchPage }
+}
