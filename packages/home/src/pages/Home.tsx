cb0302140e8c107eb7f7ec729deb6d1c551071d8
Lucas Mesquita <lucasoliveira98@outlook.com>
2021-12-14 01:48:43 -0300

    chore: testing react-helmet

--- packages/home/src/pages/Home.tsx
+++ packages/home/src/pages/Home.tsx
@@ -1,33 +1,40 @@
 import { Box, Flex, Grid, Image, Text, VStack } from '@chakra-ui/react'
+import { Helmet } from 'react-helmet'
 import Header from '../components/Header'
 
 export const HomePage = () => (
-	<Flex flexDir="column">
-		<Header />
-		<Image
-			my="16px"
-			px="16px"
-			height="200px"
-			width="100%"
-			objectFit="cover"
-			src="https://www.dommen.com.br/image/cache/catalog/banners_top/banner_novoano-2000x500w.jpg"
-			alt="Dan Abramov"
-		/>
-		<Text p="16px" fontSize="2xl">
-			Mais vendidos
-		</Text>
-		<Grid mt={8} px="16px" gap={8} gridTemplateColumns="1fr 1fr 1fr 1fr">
-			{new Array(14).fill(0).map((_, index) => (
-				<VStack spacing={8} key={index.toString()}>
-					<Box height={100} width="100%" backgroundColor="teal" />
-					<Text>
-						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora,
-						recusandae!
-					</Text>
-				</VStack>
-			))}
-		</Grid>
-	</Flex>
+	<>
+		<Helmet>
+			<meta charSet="utf-8" />
+			<title>Tela Inicial</title>
+		</Helmet>
+		<Flex flexDir="column">
+			<Header />
+			<Image
+				my="16px"
+				px="16px"
+				height="200px"
+				width="100%"
+				objectFit="cover"
+				src="https://www.dommen.com.br/image/cache/catalog/banners_top/banner_novoano-2000x500w.jpg"
+				alt="Dan Abramov"
+			/>
+			<Text p="16px" fontSize="2xl">
+				Mais vendidos
+			</Text>
+			<Grid mt={8} px="16px" gap={8} gridTemplateColumns="1fr 1fr 1fr 1fr">
+				{new Array(14).fill(0).map((_, index) => (
+					<VStack spacing={8} key={index.toString()}>
+						<Box height={100} width="100%" backgroundColor="teal" />
+						<Text>
+							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora,
+							recusandae!
+						</Text>
+					</VStack>
+				))}
+			</Grid>
+		</Flex>
+	</>
 )
 
 export default HomePage
