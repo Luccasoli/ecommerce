826e01f98887810016959f32f0fe9701731696ff
Lucas Mesquita <lucasoliveira98@outlook.com>
2021-12-21 16:40:55 -0300

    build: prod config in module federation

--- packages/host/webpack.prod.js
+++ packages/host/webpack.prod.js
@@ -1,6 +1,36 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { ModuleFederationPlugin } = require('webpack').container
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const webpackCommonConfig = require('./webpack.common')
+const { dependencies } = require('./package.json')
 
 module.exports = {
 	...webpackCommonConfig,
 	mode: 'production',
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'src', 'index.html'),
+		}),
+		new ForkTsCheckerWebpackPlugin({
+			typescript: {
+				diagnosticOptions: {
+					semantic: true,
+					syntactic: true,
+				},
+				mode: 'write-references',
+			},
+		}),
+		new ModuleFederationPlugin({
+			name: 'host',
+			remotes: {
+				home: 'home@https://home-tcc-ecommerce.netlify.app/remoteEntry.js',
+				search:
+					'search@https://search-tcc-ecommerce.netlify.app/remoteEntry.js',
+			},
+			shared: {
+				...dependencies,
+			},
+		}),
+	],
 }
--- packages/search/webpack.prod.js
+++ packages/search/webpack.prod.js
@@ -1,6 +1,38 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { ModuleFederationPlugin } = require('webpack').container
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const webpackCommonConfig = require('./webpack.common')
+const { dependencies } = require('./package.json')
 
 module.exports = {
 	...webpackCommonConfig,
 	mode: 'production',
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'src', 'index.html'),
+		}),
+		new ForkTsCheckerWebpackPlugin({
+			typescript: {
+				diagnosticOptions: {
+					semantic: true,
+					syntactic: true,
+				},
+				mode: 'write-references',
+			},
+		}),
+		new ModuleFederationPlugin({
+			name: 'search',
+			filename: 'remoteEntry.js',
+			exposes: {
+				'./Search': './src/pages/Search',
+			},
+			remotes: {
+				home: 'home@https://home-tcc-ecommerce.netlify.app/remoteEntry.js',
+			},
+			shared: {
+				...dependencies,
+			},
+		}),
+	],
 }
