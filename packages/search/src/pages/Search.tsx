dad2974a232866b5a8d7ac84a424eb95f67351e4
Lucas Mesquita <lucasoliveira98@outlook.com>
2022-03-09 01:24:04 -0300

    feat(search): adding price search by queryParam

--- packages/search/src/pages/Search.tsx
+++ packages/search/src/pages/Search.tsx
@@ -1,37 +1,79 @@
-import { Flex, Grid, HStack, IconButton, Input, Text } from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import {
+	Divider,
+	Flex,
+	Grid,
+	HStack,
+	IconButton,
+	Input,
+	Text,
+} from '@chakra-ui/react'
 import { ProductItemCard } from '@home/ProductItemCard'
 import { useFetch } from '@home/useFetch'
 import Header from '@host/Header'
-import { useLocation } from 'react-router-dom'
-import { ChevronRightIcon } from '@chakra-ui/icons'
+import { SyntheticEvent } from 'react'
+import { useLocation, useSearchParams } from 'react-router-dom'
 
-const Filters = () => (
-	<Flex
-		bg="white"
-		borderRadius="lg"
-		borderWidth="1px"
-		boxShadow="md"
-		flex="1"
-		flexDir="column"
-		minH="50vh"
-		overflow="hidden"
-		p={4}
-	>
-		<Text fontSize="2xl">Preço</Text>
-		<Flex alignItems="center" gap="2">
-			<Input placeholder="Mínimo" size="sm" />
-			<Input placeholder="Máximo" size="sm" />
-			<IconButton
-				aria-label="filter-price"
-				colorScheme="teal"
-				icon={<ChevronRightIcon />}
-				onClick={() => {}}
-				size="sm"
-				variant="outline"
-			/>
+const Filters = () => {
+	const [searchParams, setSearchParams] = useSearchParams()
+
+	const onFilterPrice = (e: SyntheticEvent) => {
+		e.preventDefault()
+		const target = e.target as typeof e.target & {
+			min: { value?: string }
+			max: { value?: string }
+		}
+
+		searchParams.delete('min')
+		searchParams.delete('max')
+
+		if (target.min.value) {
+			searchParams.set('min', target.min.value)
+		}
+
+		if (target.max.value) {
+			searchParams.set('max', target.max.value)
+		}
+
+		setSearchParams(searchParams)
+	}
+
+	return (
+		<Flex
+			bg="white"
+			borderRadius="lg"
+			borderWidth="1px"
+			boxShadow="md"
+			flex="1"
+			flexDir="column"
+			minH="50vh"
+			overflow="hidden"
+			p={4}
+		>
+			<Flex alignItems="center" mb="2">
+				<Text fontSize="lg">Preço</Text>{' '}
+				<Divider borderTop="1px solid teal" ml="2" />
+			</Flex>
+			<Flex alignItems="center" as="form" gap="2" onSubmit={onFilterPrice}>
+				<Input defaultValue="25" name="min" placeholder="Mínimo" size="sm" />
+				<Input
+					defaultValue="123.23"
+					name="max"
+					placeholder="Máximo"
+					size="sm"
+				/>
+				<IconButton
+					aria-label="filter-price"
+					colorScheme="teal"
+					icon={<ChevronRightIcon />}
+					size="sm"
+					type="submit"
+					variant="outline"
+				/>
+			</Flex>
 		</Flex>
-	</Flex>
-)
+	)
+}
 
 export const SearchPage = () => {
 	const { data, loading, error } = useFetch(
