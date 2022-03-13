ceaeebc05eba918c124f00dba45862a00961e584
Lucas Mesquita <lucasoliveira98@outlook.com>
2022-03-13 17:25:59 -0300

    fix(auth): allowing works independently

--- packages/auth/src/App.tsx
+++ packages/auth/src/App.tsx
@@ -1,16 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { CartProvider } from '@host/CartProvider'
 import { AuthPage } from './pages/Auth'
 
 const DEFAULT_PATH = '/auth'
 
 export const App = () => (
 	<ChakraProvider>
-		<BrowserRouter>
-			<Routes>
-				<Route path={DEFAULT_PATH} element={<AuthPage />} />
-				<Route path="*" element={<Navigate to={DEFAULT_PATH} />} />
-			</Routes>
-		</BrowserRouter>
+		<CartProvider>
+			<BrowserRouter>
+				<Routes>
+					<Route element={<AuthPage />} path={DEFAULT_PATH} />
+					<Route element={<Navigate to={DEFAULT_PATH} />} path="*" />
+				</Routes>
+			</BrowserRouter>
+		</CartProvider>
 	</ChakraProvider>
 )
