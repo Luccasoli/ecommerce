c5c3ac16c8e391299dcaad0ba627d23af0e5d8d0
Lucas Mesquita <lucasoliveira98@outlook.com>
2022-04-03 15:28:20 -0300

    feat(adonis): address migration/model

---
+++ packages/backend/database/migrations/1649010305586_addresses.ts
@@ -0,0 +1,29 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Addresses extends BaseSchema {
+  protected tableName = 'addresses'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('address').notNullable()
+      table.string('number').notNullable()
+      table.string('complement')
+      table.string('neighborhood').notNullable()
+      table.string('city').notNullable()
+      table.string('state').notNullable()
+      table.string('postal_code').notNullable()
+      table.string('country').notNullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
