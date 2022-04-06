97a24693c9f0ab359f3c14c9b7363565873fdf15
Lucas Mesquita <lucasoliveira98@outlook.com>
2022-04-05 23:26:55 -0300

    temp

--- packages/backend/database/factories/index.ts
+++ packages/backend/database/factories/index.ts
@@ -1,1 +1,46 @@
-// import Factory from '@ioc:Adonis/Lucid/Factory'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+import ProductCategory from 'App/Models/ProductCategory'
+import Product from 'App/Models/Product'
+import ProductImage from 'App/Models/ProductImage'
+
+function randomIntFromInterval(max: number, min: number = 1) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+export const ProductCategorieFactory = Factory.define(ProductCategory, ({ faker }) => {
+  const name = faker.commerce.department()
+
+  return {
+    image_alt: name,
+    image_url: faker.image.imageUrl(),
+    name: name,
+    category: faker.commerce.department(),
+  }
+}).build()
+
+export const ProductFactory = Factory.define(Product, async ({ faker }) => {
+  const name = faker.commerce.productName()
+  const costPrice = Math.floor(Math.random() * 10000)
+
+  return {
+    name,
+    description: faker.commerce.productDescription(),
+    cost_price: costPrice,
+    selling_price: Math.floor(costPrice * (Math.random() + 1)),
+    image_alt: name,
+    image_url: faker.image.imageUrl(),
+    // pick a random category
+    category: await ProductCategory.find(randomIntFromInterval(10)),
+  }
+}).build()
+
+export const ProductImageFactory = Factory.define(ProductImage, async ({ faker }) => {
+  const name = faker.commerce.department()
+
+  return {
+    image_alt: name,
+    image_url: faker.image.imageUrl(),
+    product: await ProductCategory.find(randomIntFromInterval(30)),
+  }
+}).build()
--- packages/backend/database/seeders/ProductCategory.ts
+++ packages/backend/database/seeders/ProductCategory.ts
@@ -1,22 +1,8 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-import { randImg, randProductCategory } from '@ngneat/falso'
-import ProductCategory from 'App/Models/ProductCategory'
+import { ProductCategorieFactory } from 'Database/factories'
 
 export default class ProductCategorySeeder extends BaseSeeder {
   public async run() {
-    await ProductCategory.createMany([
-      ...Array(10)
-        .fill(null)
-        .map(() => {
-          const name = randProductCategory()
-
-          return {
-            name,
-            description: '',
-            image_alt: randImg(),
-            image_url: name,
-          }
-        }),
-    ])
+    await ProductCategorieFactory.createMany(10)
   }
 }
