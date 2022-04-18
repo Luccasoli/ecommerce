bc98337237b1d356be909386b90ff8cec27ea51b
Lucas Mesquita <lucasoliveira98@outlook.com>
2022-04-17 22:32:01 -0300

    feat(adonis): list products paginated

--- packages/backend/app/Controllers/Http/ProductsController.ts
+++ packages/backend/app/Controllers/Http/ProductsController.ts
@@ -1,9 +1,20 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
 import Product from 'App/Models/Product'
 
 export default class ProductsController {
-  public async index() {
-    return Product.all()
+  public async index(ctx: HttpContextContract) {
+    const { limit, page } = ctx.request.params() as {
+      limit?: number
+      page?: number
+    }
+
+    const payload = await Product.query().paginate(page || 1, limit || 20)
+
+    return ctx.response.status(200).json({
+      message: 'List of products',
+      payload,
+    })
   }
 
   public async show({ params }: HttpContextContract) {
